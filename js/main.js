a34c062292168289061320df045d3b9ffb4cd685
ShayIsso <shayisso@gmail.com>
2024-10-26 19:34:29 +0300

    Add size limit to onBallClick

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ var ballSize = 100
 
 function onBallClick(elCell) {
   ballSize += 50
+
+  if (ballSize > 400) ballSize = 100
+
   elCell.style.width = `${ballSize}px`
   elCell.style.height = `${ballSize}px`
+  elCell.innerText = ballSize
 }
