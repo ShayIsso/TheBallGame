3c7cbc27176cefa1b9fd36068410943cb187f338
ShayIsso <shayisso@gmail.com>
2024-10-26 20:00:25 +0300

    Add random color to ball clicks

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ var ballSize = 100
 function onBallClick(elCell) {
   const ranIncrease = getRandomInt(20, 60)
   ballSize += ranIncrease
-  
+
   if (ballSize > 400) ballSize = 100
 
   elCell.style.width = `${ballSize}px`
   elCell.style.height = `${ballSize}px`
+  elCell.style.backgroundColor = getRandomColor()
   elCell.innerText = ballSize
 }
--- js/util.js
+++ js/util.js
@@ -3,3 +3,12 @@ function getRandomInt(min, max) {
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
   }
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
