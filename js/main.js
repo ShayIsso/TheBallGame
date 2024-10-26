682f30b0166d7f8bdde07d132b85a830380aeb2f
ShayIsso <shayisso@gmail.com>
2024-10-26 19:11:42 +0300

    Simplify onBallClick size calculation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict'
+var ballSize = 100
 
-function onBallClick() {
-  console.log('Ball clicked')
+function onBallClick(elCell) {
+  ballSize += 50
+  elCell.style.width = `${ballSize}px`
+  elCell.style.height = `${ballSize}px`
 }
