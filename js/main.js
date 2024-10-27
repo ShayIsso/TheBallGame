0a714fd6dc5455d7fbfdeb1e9f52c637d0b6d032
ShayIsso <shayisso@gmail.com>
2024-10-27 21:07:28 +0200

    Add swap functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 'use strict'
-var ballSize = 100
 
 function onBallClick(elCell, madDiameter) {
   const ranIncrease = getRandomInt(20, 60)
-  ballSize += ranIncrease
+  var size = +(elCell.innerText) + ranIncrease 
 
-  if (ballSize > madDiameter) ballSize = 100
+  if (size > madDiameter) size = 100
 
-  elCell.style.width = `${ballSize}px`
-  elCell.style.height = `${ballSize}px`
+  elCell.style.width = `${size}px`
+  elCell.style.height = `${size}px`
   elCell.style.backgroundColor = getRandomColor()
-  elCell.innerText = ballSize
+  elCell.innerText = size
+}
+
+function swapBalls() {
+  const ball1 = document.querySelector('.ball-1');
+  const ball2 = document.querySelector('.ball-2');
+  
+  const tempColor = ball1.style.backgroundColor;
+  const tempSize = ball1.innerText;
+  
+  ball1.style.backgroundColor = ball2.style.backgroundColor;
+  ball1.innerText = ball2.innerText;
+  ball1.style.width = ball2.style.width;
+  ball1.style.height = ball2.style.height;
+  
+  ball2.style.backgroundColor = tempColor;
+  ball2.innerText = tempSize;
+  ball2.style.width = `${tempSize}px`;
+  ball2.style.height = `${tempSize}px`;
 }
