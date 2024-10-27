d342576a561c24d86eed0bb17c5574d0c3549655
ShayIsso <shayisso@gmail.com>
2024-10-27 20:33:20 +0200

    Add maxDiameter param to onBallClick

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict'
 var ballSize = 100
 
-function onBallClick(elCell) {
+function onBallClick(elCell, madDiameter) {
   const ranIncrease = getRandomInt(20, 60)
   ballSize += ranIncrease
 
-  if (ballSize > 400) ballSize = 100
+  if (ballSize > madDiameter) ballSize = 100
 
   elCell.style.width = `${ballSize}px`
   elCell.style.height = `${ballSize}px`
