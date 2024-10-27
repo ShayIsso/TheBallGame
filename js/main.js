dd133e46ce35ac4dc425d139f93629cf76a9d0c7
ShayIsso <shayisso@gmail.com>
2024-10-27 21:48:20 +0200

    Add reduceBalls functionality

--- js/main.js
+++ js/main.js
@@ -29,3 +29,20 @@ function swapBalls() {
   ball2.style.width = `${tempSize}px`;
   ball2.style.height = `${tempSize}px`;
 }
+
+function reduceBalls() {
+  const ball1 = document.querySelector('.ball-1')
+  const ball2 = document.querySelector('.ball-2')
+  
+  const reduction = getRandomInt(20, 60);
+  
+  var size1 = Math.max(100, +(ball1.innerText) - reduction)
+  ball1.innerText = size1
+  ball1.style.width = `${size1}px`
+  ball1.style.height = `${size1}px`
+  
+  var size2 = Math.max(100, +(ball2.innerText) - reduction)
+  ball2.innerText = size2;
+  ball2.style.width = `${size2}px`;
+  ball2.style.height = `${size2}px`;
+}
